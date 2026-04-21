799589234152708618e3bb9cf220084a359e29d5
Sanjna kori <sanjanakori470@gmail.com>
2026-04-22 03:11:38 +0530

    Deployment setup

--- ExamDutyManager/settings.py
+++ ExamDutyManager/settings.py
@@ -100,16 +100,18 @@ LOGOUT_REDIRECT_URL = "/accounts/login/"
 # ─────────────────────────────────────
 
 # Development — see emails in terminal
-EMAIL_BACKEND     = 'django.core.mail.backends.console.EmailBackend'
-DEFAULT_FROM_EMAIL = 'ExamDutyManager <noreply@examduty.com>'
+# EMAIL_BACKEND     = 'django.core.mail.backends.console.EmailBackend'
+# DEFAULT_FROM_EMAIL = 'ExamDutyManager <noreply@examduty.com>'
 
 # ─────────────────────────────────────
 # Production — uncomment when ready
 # ─────────────────────────────────────
-# EMAIL_BACKEND       = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST          = 'smtp.gmail.com'
-# EMAIL_PORT          = 587
-# EMAIL_USE_TLS       = True
-# EMAIL_HOST_USER     = 'your_email@gmail.com'
-# EMAIL_HOST_PASSWORD = 'your_16_digit_app_password'
-# DEFAULT_FROM_EMAIL  = 'ExamDutyManager <your_email@gmail.com>'
+EMAIL_BACKEND       = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST          = 'smtp.gmail.com'
+EMAIL_PORT          = 587
+EMAIL_USE_TLS       = True
+EMAIL_HOST_USER     = 'your_email@gmail.com'
+EMAIL_HOST_PASSWORD = 'your_16_digit_app_password'
+DEFAULT_FROM_EMAIL  = 'ExamDutyManager <your_email@gmail.com>'
+
+ALLOWED_HOSTS = ['*']
